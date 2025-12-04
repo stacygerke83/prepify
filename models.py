3e575e36ab0a2629d61eea02c7baadc5886633e3
Stacy Gerke <stacy.gerke83@gmail.com>
2025-12-04 11:38:05 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,15 @@
+# prepify/models.py
 from . import db
 
 class PantryItem(db.Model):
     __tablename__ = 'pantry_items'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, index=True)
-    quantity = db.Column(db.String(50))
-    category = db.Column(db.String(50))
+    # Keeping quantity as String to avoid migration for now.
+    # If you want numeric later, change to Integer + a migration.
+    quantity = db.Column(db.String(50), nullable=True, default='')
+    category = db.Column(db.String(50), nullable=True, default='')
+
+    def __repr__(self) -> str:
+        return f"<PantryItem id={self.id} name={self.name!r}>"
